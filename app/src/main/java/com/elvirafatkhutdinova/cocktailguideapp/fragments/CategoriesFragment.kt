e5beb55f52bd4e82259568a4c0ca107dcf43b31d
elvirahoward <29987317+ElviraHoward@users.noreply.github.com>
2023-04-17 19:58:08 +0400

    basic onBoarding is set

--- app/src/main/java/com/elvirafatkhutdinova/cocktailguideapp/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/elvirafatkhutdinova/cocktailguideapp/fragments/CategoriesFragment.kt
@@ -1,10 +1,8 @@
 package com.elvirafatkhutdinova.cocktailguideapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.elvirafatkhutdinova.cocktailguideapp.R
 
 class CategoriesFragment : Fragment(R.layout.fragment_categories) {
---
+++ app/src/main/java/com/elvirafatkhutdinova/cocktailguideapp/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,42 @@
+package com.elvirafatkhutdinova.cocktailguideapp.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.elvirafatkhutdinova.cocktailguideapp.databinding.ActivityOnboardingBinding
+
+private const val NUM_PAGES = 3
+
+class OnBoardingActivity : FragmentActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+    private lateinit var viewPager : ViewPager2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        viewPager = binding.viewPager
+        val adapter = SlidePagerAdapter(this)
+        viewPager.adapter = adapter
+        setContentView(view)
+    }
+
+    override fun onBackPressed() {
+        if (viewPager.currentItem == 0) {
+            super.onBackPressed()
+        } else {
+            viewPager.currentItem = viewPager.currentItem - 1
+        }
+    }
+
+    private inner class SlidePagerAdapter(f: FragmentActivity) : FragmentStateAdapter(f) {
+
+        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun createFragment(position: Int): Fragment = OnBoardingFragment()
+    }
+}
